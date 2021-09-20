30a02fa6470e49d8aee29f61706435bf997c7477
KavindaLochana <kaviduljayasekara@gmail.com>
2021-09-20 15:09:48 +0530

    decoration

--- lib/googleSignIn/googleSignInPage.dart
+++ lib/googleSignIn/googleSignInPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:imedirep/googleSignIn/googleSignInProvider.dart';
 
@@ -9,26 +10,46 @@ class SiginInPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.teal,
         title: Text('Welcome!'),
+        centerTitle: true,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Center(
-            child: Container(
-              child: Text('Test'),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Text(
+                  'Please sign in with your google account..',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              ),
             ),
           ),
-          Center(
-            child: ElevatedButton(
-              child: Text('Sign Up With Google'),
-              //signin button
-              onPressed: () {
-                final provider =
-                    Provider.of<GoogleSignInProvider>(context, listen: false);
-                provider.googleLogin();
-              },
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: ElevatedButton.icon(
+                icon: FaIcon(
+                  Icons.login,
+                  color: Colors.white,
+                ),
+                label: Text('Sign Up With Google'),
+                //signin button
+                onPressed: () {
+                  final provider =
+                      Provider.of<GoogleSignInProvider>(context, listen: false);
+                  provider.googleLogin();
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  onPrimary: Colors.white,
+                  minimumSize: Size(double.infinity, 50),
+                ),
+              ),
             ),
           ),
         ],
